acfa3d35ede89475ede5f2f1cb5355ae832b669c
rsp9u <gctwt852@yahoo.co.jp>
2019-05-08 21:24:16 +0900

    Add an example

---
+++ example/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/rsp9u/go-xlsshape/oxml"
+	"github.com/rsp9u/go-xlsshape/oxml/shape"
+)
+
+func main() {
+	ss := oxml.NewSpreadsheet()
+
+	rect := shape.NewRectangle()
+	rect.SetLeftTop(20, 20)
+	rect.SetSize(300, 150)
+	rect.SetText("A shape with text", "en-US")
+	ss.AddShape(rect)
+
+	rect2 := shape.NewRectangle()
+	rect2.SetLeftTop(320, 20)
+	rect2.SetSize(300, 150)
+	rect2.SetNoFill(true)
+	rect2.SetNoLine(true)
+	rect2.SetText("A shape with text\nWithout fill and line", "en-US")
+	ss.AddShape(rect2)
+
+	line1 := shape.NewLine()
+	line1.SetLeftTop(20, 300)
+	line1.SetRightBottom(300, 300)
+	line1.SetTailType("arrow")
+	ss.AddShape(line1)
+
+	line2 := shape.NewLine()
+	line2.SetLeftTop(20, 340)
+	line2.SetRightBottom(300, 340)
+	line2.SetDashType("dash")
+	line2.SetHeadType("triangle")
+	line2.SetTailType("stealth")
+	ss.AddShape(line2)
+
+	line3 := shape.NewLine()
+	line3.SetLeftTop(20, 380)
+	line3.SetRightBottom(300, 380)
+	line3.SetDashType("sysDot")
+	ss.AddShape(line3)
+
+	ss.Dump("example1.xlsx")
+}
--- oxml/spreadsheet.go
+++ oxml/spreadsheet.go
@@ -3,6 +3,8 @@ package oxml
 import (
 	"log"
 	"os"
+
+	"github.com/rsp9u/go-xlsshape/oxml/shape"
 )
 
 // Spreadsheet creates a consumable Excel file.
@@ -13,6 +15,7 @@ type Spreadsheet struct {
 	worksheets   []*Worksheet
 	coreprops    *CoreProps
 	appprops     *AppProps
+	drawing      *Drawing
 }
 
 // NewSpreadsheet creates a new spread sheet.
@@ -24,6 +27,7 @@ func NewSpreadsheet() *Spreadsheet {
 	ct.AddOverride(OverrideType{PartName: "/xl/worksheets/sheet1.xml", ContentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"})
 	ct.AddOverride(OverrideType{PartName: "/xl/theme/theme1.xml", ContentType: "application/vnd.openxmlformats-officedocument.theme+xml"})
 	ct.AddOverride(OverrideType{PartName: "/xl/styles.xml", ContentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml"})
+	ct.AddOverride(OverrideType{PartName: "/xl/drawings/drawing1.xml", ContentType: "application/vnd.openxmlformats-officedocument.drawing+xml"})
 	ct.AddOverride(OverrideType{PartName: "/docProps/core.xml", ContentType: "application/vnd.openxmlformats-package.core-properties+xml"})
 	ct.AddOverride(OverrideType{PartName: "/docProps/app.xml", ContentType: "application/vnd.openxmlformats-officedocument.extended-properties+xml"})
 
@@ -33,6 +37,10 @@ func NewSpreadsheet() *Spreadsheet {
 	wb := NewWorkbook("xl/workbook.xml")
 	ws := []*Worksheet{NewWorksheet("xl/worksheets/sheet1.xml")}
 	wb.Add("Sheet1", "1", ws[0])
+	ws[0].SetDefaultCellSize("2.5", "15")
+
+	drawing := NewDrawing("xl/drawings/drawing1.xml")
+	ws[0].AddDrawing(drawing)
 
 	rel := NewRelationships("_rels/.rels")
 	rel.Add(Relationship{ID: "rId1", Type: typeRelationshipsDocument, Target: wb.Path()})
@@ -46,25 +54,32 @@ func NewSpreadsheet() *Spreadsheet {
 	p.Add(appProps)
 	p.Add(wb)
 	p.Add(wb.Relationships())
+	p.Add(drawing)
 	for _, sheet := range ws {
 		p.Add(sheet)
+		p.Add(sheet.Relationships())
 	}
 
-	return &Spreadsheet{&p, ct, wb, ws, coreProps, appProps}
+	return &Spreadsheet{&p, ct, wb, ws, coreProps, appProps, drawing}
 }
 
 // List lists the all paths in this file.
-func (s *Spreadsheet) List() []string {
-	return s.pkg.List()
+func (ss *Spreadsheet) List() []string {
+	return ss.pkg.List()
 }
 
 // Dump writes out the contents into the new Excel file
-func (s *Spreadsheet) Dump(filename string) {
-	buf, _ := s.pkg.Packaging()
+func (ss *Spreadsheet) Dump(filename string) {
+	buf, _ := ss.pkg.Packaging()
 	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 	file.Write(buf.Bytes())
+}
+
+// AddShape adds a shape into the drawing collection of this spreadsheet.
+func (ss *Spreadsheet) AddShape(s shape.Shape) {
+	ss.drawing.AddShape(s)
 }
